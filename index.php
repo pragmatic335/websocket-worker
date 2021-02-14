7773395340063f7fb4ca8cff504f35d723616cf9
pragmatic335 <pragmatik335@gmail.com>
2021-02-15 01:00:51 +0700

    ready simple websocket for test

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 
-    echo 'hi websocket!';
+$worker = new \Workerman\Worker('websocket://0.0.0.0:8001');
+$worker->count = 4;
+
+$worker->onConnect = function ($connection) {
+  $connection->send('message');
+ \Workerman\Lib\Timer::add(1, function() use($connection) {
+     $connection->send('Hello from server');
+ });
+};
+
+$worker->onMessage = function($connection, $data) {
+   $connection->send($data);
+};
+
+
+
+\Workerman\Worker::runAll();
+
 
